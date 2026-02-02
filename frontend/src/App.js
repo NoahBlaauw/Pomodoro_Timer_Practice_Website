ee2edce2b1ebc2a1758919ca3356e9923d504435
NoahBlaauw <601195@student.belgiumcampus.ac.za>
2026-02-02 21:04:54 +0200

    Added clock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
@@ -20,16 +20,58 @@ function App() {
     .then(val => {setJunk(val)})
   }
 
-  return (
-    <div className="App">
-      <h1>List O Junk</h1>
-      <ul>
-        {junkToList()}
-      </ul>
+  // return (
+  //   <div className="App">
+  //     <h1>List O Junk</h1>
+  //     <ul>
+  //       {junkToList()}
+  //     </ul>
 
-      <button onClick = {handleClick}>Click here to get backend junk</button>
-    </div>
-  );
+  //     <button onClick = {handleClick}>Click here to get backend junk</button>
+  //   </div>
+  // );
+
+  //-----------------------------------------------clock stuff
+  const [time, setTime] = useState('');
+
+function FormatTime(val){
+  if(val < 10){
+    return '0'
+  }else{
+    return '';
+  }
+} //used to add 0s to the start of single digit numbers
+
+useEffect(() => {
+  tick();
+  const timerID = setInterval(() => {
+    const d = new Date();
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+
+    setTime(FormatTime(h) + h + ":" + FormatTime(m) + m + ":" + FormatTime(s) + s);
+  }, 1000);
+
+  return () => clearInterval(timerID);
+}, [])
+
+function tick(){
+
+
+}
+
+return(
+  <div className = 'clock'>
+    <h1 className = "Time">{time}</h1>
+  </div>
+)
+
+
+
 }
 
 export default App;
+
+
+
