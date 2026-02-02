53007c51d53a7d3adf7750dffa6231f4ec5ea9d1
NoahBlaauw <601195@student.belgiumcampus.ac.za>
2026-02-02 18:01:25 +0200

    Installed nodemon and tested routes

--- backend/Server.js
+++ backend/Server.js
@@ -8,4 +8,11 @@ app.get('/junk', (req,res) => {
     res.json(junk);
 });
 
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+    console.log("Home");
+    res.send("Helloooooo");
+})//Most basic route. Basically the home page
+
+
 app.listen(port);
